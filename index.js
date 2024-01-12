1b12e12a211680be858675894f9bfbd5b4e6f1f7
anfal11 <anfal15-14303@diu.edu.bd>
2024-01-13 03:03:37 +0600

    multer has been implememnted

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const app = express();
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 const port = process.env.PORT || 5000;
 
 //middleware
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.cb3wozl.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -25,9 +29,52 @@ async function run() {
 
     const filesCOllection = client.db("fileUpload").collection("files");
 
-    
+    const storage = multer.diskStorage({
+      destination: function(req, file, cb){
+        return cb(null, "./Files/Files")
+      },
+      filename: (req, file, cb) =>{
+        return cb(null, `${Date.now()}_${file.originalname}`)
+      }
+    })
+
+    // multer middleware
+    const upload = multer({
+      storage: storage,
+      limits: {
+        fileSize: 1000000
+      }
+    })
+
+    app.post("/upload", upload.single('file'), async (req, res) => {
+      console.log(req.body);
+      console.log(req.file);
+      const newImg = req.file;
+      const encImg = newImg.toString('base64');
+
+      const image = {
+        contentType: newImg.mimetype,
+        size: newImg.size,
+        img: Buffer.from(encImg, 'base64')
+      }
+
+      const result = await filesCOllection.insertOne(image);
+      console.log(result);
+      if(result){
+        res.send(result)
+      }
+      else{
+        res.send(false)
+      }
 
 
+    })
+
+    app.get("/upload", async (req, res) => {
+      const result = await filesCOllection.find({});
+      const data = await result.toArray();
+      res.send(data);
+    })
 
 
     
